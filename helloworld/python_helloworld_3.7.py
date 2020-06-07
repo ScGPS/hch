c90d56e5de7a541981819e9d0e32809246a65f82
hch201205 <hch201205@126.com>
2020-06-07 10:04:56 +0800

    ok

--- helloworld/python_helloworld_3.7.py
+++ helloworld/python_helloworld_3.7.py
@@ -1,2 +1,9 @@
 # -*- coding: UTF-8 -*-
+import tensorflow as tf
+print(tf.__version__)
 print('Hello World Welcome!') 
+print(5+3)
+print(9999999999999999999999999999999999999999999+87654321)
+print(5*3)
+print(2345*6789)
+print(1234567898765432123456789*9876543212345678987654321)
