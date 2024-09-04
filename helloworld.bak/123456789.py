1a98a4a0762fe948b78a066cb89fb22e9cf2dc49
ScGPS <happy.every.day@126.com>
2024-09-04 09:59:00 +0800

    commit codes

---
+++ helloworld.bak/123456789.py
@@ -0,0 +1,18 @@
+import random
+secret = random.randint(1,99)
+guess = 0
+tries = 0
+print("AHOY! I'm the Dread Pirate Robeerts, and I have a secret!")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ helloworld.bak/python_for.py
@@ -0,0 +1,12 @@
+for i in range(1,50):
+    print('*****************************************************')
+
+print('')
+
+
+
+for i in range(1,10):
+    print('')
+    for j in range(1,i+1):
+        print('*',end='')
+print('')
